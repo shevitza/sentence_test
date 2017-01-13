ecafcaeb6791df2ca2c9aa1dad5af278487fe95b
Evgenia Hristova <info@shevitza.com>
2017-01-13 07:42:54 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,33 +1,42 @@
-var app = angular.module("app", []);
-app.controller("FormController", ['$scope', function ($scope) {
-    $scope.left ='Left1';
-    $scope.middle ='Mid4';
-    $scope.right ='Right6';
+(function () {
+    "use strict";
+    angular.module("app", [])
+        .controller("FormController", FormController);
+    FormController.$inject = ['$scope', 'sentenceFactory'];
 
-    $scope.guess = "";
-    var correctAnswers = ['Left1 Mid3 Right7', 'Left4 Mid4 Right8', 'Left3 Mid4 Right6'];
+    function FormController($scope, sentenceFactory) {
+        var list = sentenceFactory.list;
 
-    $scope.checkResult = function () {
-        console.log('check');
-        var i;
-        var checked = $scope.left + ' ' + $scope.middle + ' ' + $scope.right;
-        if (correctAnswers.length === 0) {
-            $scope.result = "No more sentences for guess.";
-        } else {
-            if (correctAnswers.indexOf(checked) === -1) {
-                $scope.result = "Wrong answer!";
+        //initial selection must be random!
+        $scope.left =sentenceFactory.initial.left;
+        $scope.middle =sentenceFactory.initial.middle;
+        $scope.right =sentenceFactory.initial.right;
+
+        $scope.guess = "";
+    
+        var correctAnswers = sentenceFactory.answers;
+        $scope.checkResult = function () {
+            var i;
+            var sentence = list.get($scope.left) + ' ' + list.get($scope.middle) + ' ' + list.get($scope.right);
+            var checked = $scope.left + ' ' + $scope.middle + ' ' + $scope.right;
+            console.log(checked);
+            if (correctAnswers.length === 0) {
+                $scope.result = "No more sentences for guess.";
             } else {
-                i = correctAnswers.indexOf(checked);
-                correctAnswers.splice(i, 1);
-                $scope.guess = $scope.guess + "-->" + checked;
-                $scope.result = "Congratulations! Correct!!";
+                if (correctAnswers.indexOf(checked) === -1) {
+                    $scope.result = "Wrong answer!";
+                } else {
+                    i = correctAnswers.indexOf(checked);
+                    correctAnswers.splice(i, 1);
+                    $scope.guess = $scope.guess + " " + sentence;
+                    $scope.result = "Congratulations! Correct!!";
+                }
             }
         }
-
-
     }
 
-}]);
+})();
+
 
 
 
